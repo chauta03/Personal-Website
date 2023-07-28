903dbd15685f2c0a8f3319551c830c38bbaf1069
tachau03 <taminhchau8903@gmail.com>
2023-07-28 10:33:13 -0400

    intro

--- chaus-website/src/App.js
+++ chaus-website/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from "./components/header";
+import Intro from "./components/intro";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Intro />
     </div>
   );
 }
---
+++ chaus-website/src/components/intro.jsx
@@ -0,0 +1,15 @@
+import './intro.css';
+const Intro = () => {
+  return (
+    <div className="main-container">
+      <div className="text1">
+        Text 1
+      </div>
+      <div className="text2">
+        Text 2
+      </div>
+    </div>
+  );
+};
+
+export default Intro;
