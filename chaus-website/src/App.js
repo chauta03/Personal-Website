bcb857c4ea66d93a045d984c7831cb1f85f51dc3
tachau03 <taminhchau8903@gmail.com>
2023-07-28 17:12:47 -0400

    Update App.js

--- chaus-website/src/App.js
+++ chaus-website/src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
 import Header from "./components/header";
-import Intro from "./components/intro";
-import Projects from './components/projects';
+import Main from './pages/main';
+import Footer from './components/footer';
+
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Intro />
-      <Projects />
+      <Main />
+      <Footer />
     </div>
   );
 }
