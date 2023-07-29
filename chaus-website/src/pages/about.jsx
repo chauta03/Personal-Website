6cd409b65d89f3350d3440bfeff03c5749125b6b
tachau03 <taminhchau8903@gmail.com>
2023-07-29 15:43:17 -0400

    about page

---
+++ chaus-website/src/pages/about.jsx
@@ -0,0 +1,15 @@
+import './about.css';
+const About = () => {
+  return (
+    <div className="main-container">
+      <div className="text1">
+        Bring impacts to life
+      </div>
+      <div className="text2">
+        I work on purpose-driven projects as software engineer that have positive impacts on the companies. <a>Currently looking for an internship of Summer 2024.</a>
+      </div>
+    </div>
+  );
+};
+
+export default About;
