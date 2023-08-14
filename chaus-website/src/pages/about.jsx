b1884382fcdec7a9d43dc5563045190eefa891ca
tachau03 <taminhchau8903@gmail.com>
2023-08-14 21:16:29 +0200

    fix about page

--- chaus-website/src/pages/about.jsx
+++ chaus-website/src/pages/about.jsx
@@ -3,7 +3,7 @@ import me from '../images/me/me.jpeg';
 
 const Experience = (props) => {
   return (
-    <div className='exp-container'>
+    <div className='exp-container' style={props.style}>
       <div className='exp-position'>{props.position}, {props.company}</div>
       <div className='exp-time'>{props.time}</div>
       <div className='exp-des'>{props.description}</div>
@@ -13,10 +13,10 @@ const Experience = (props) => {
 
 const About = () => {
   const exps = [
-    { position: "Research Assistant", company: 'Kalamazoo College', time: 'June 2023 - Current', description: 'Upgrading Kit Management System for CS department focused on grading, feedback, and cloning assignment'},
-    { position: "Software Developer", company: 'Develop For Good', time: 'May 2023 - Current', description: 'Build browser-based UI learning tools Jara Unit for children with minimal internet and classroom access and teaching tools for teachers during self-study time'},
-    { position: "Teaching Assistant", company: 'Kalamazoo College', time: 'Sep 2022 - Current', description: 'Grading and conducting office hours at CS Collaboration Center to help students with functional programming, data structures, memory organization assignments'},
-    { position: "Software Developer Intern", company: 'MISA Joint Stock Company', time: 'Jul - Aug 2022', description: 'Developed backend architecture for link shortener tool in website format to replace old system'},
+    { position: "Research Assistant", company: 'Kalamazoo College', time: 'June 2023 - Current', description: 'Upgrading Kit Management System for CS department focused on grading, feedback, and cloning assignment' },
+    { position: "Software Developer", company: 'Develop For Good', time: 'May 2023 - Current', description: 'Build browser-based UI learning tools Jara Unit for children with minimal internet and classroom access and teaching tools for teachers during self-study time' },
+    { position: "Teaching Assistant", company: 'Kalamazoo College', time: 'Sep 2022 - Current', description: 'Grading and conducting office hours at CS Collaboration Center to help students with functional programming, data structures, memory organization assignments' },
+    { position: "Software Developer Intern", company: 'MISA Joint Stock Company', time: 'Jul - Aug 2022', description: 'Developed backend architecture for link shortener tool in website format to replace old system' },
   ];
 
   return (
@@ -38,12 +38,46 @@ const About = () => {
       </div>
       <div className='skills-container'>
         <div className='skills-container-left'>
-
+          <div className='education'>
+            <div className='skill-header'>----</div>
+            <div className='skill-header'>Education</div>
+            <Experience
+              position='Kalamazoo College, MI'
+              time='2021 - 2025'
+              description='B.A. Computer Science and Mathematics'
+            />
+          </div>
+          <div className='skills'>
+            <div className='skill-header'>Skills</div>
+            <Experience
+              position='Tools & Frameworks'
+              description='Python, Java, C, Git, JavaScript, TypeScript, HTML/CSS, React, FastAPI, MongoDB, ElasticSearch, Docker, MySQL'
+              style={{ display: 'flex', gap: '10px'}} />
+            <div style={{ display: 'flex', flexDirection:'column', gap: '20px'}}>
+              <div className='exp-position' >Revelant Training</div>
+              <div style={{ display: 'flex', flexDirection:'column', gap: '15px'}}>
+                <div className='exp-des'>AWS AI & ML Scholarship program: AI Programming with Python Nanodegree</div>
+                <div className='exp-des'>Harvard WECode Conference with WeAmplify Extended Scholarship</div>
+                <div className='exp-des'>CodePath Certificate in Intermediate Software Engineering</div>
+                <div className='exp-des'>Harvard University Certificate in CS50x Intro to Programming</div>
+              </div>
+            </div>
+          </div>
         </div>
         <div className='skills-container-right'>
-            <div>Experience</div>
-            <div>
-            </div>
+          <div className='skill-header'>Experience</div>
+          <div className='skills-container-right-small'>
+            {exps.map((exp) => {
+              return (
+                <Experience
+                  position={exp.position}
+                  company={exp.company}
+                  time={exp.time}
+                  description={exp.description}
+                />
+              );
+            })}
+          </div>
         </div>
       </div>
     </div>
