ea998f9be1b880b66d4857957d655a09bce0ac72
tachau03 <taminhchau8903@gmail.com>
2023-08-22 17:20:12 +0200

    fix profile link

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -16,7 +16,7 @@ const Header = () => {
         {/* <Link to='projects' className="button" style={{ color:'black'}}>
           PROJECTS
         </Link> */}
-        <a href="https://drive.google.com/file/d/1wqyyXZmoXeLTk9giEtWCVXwMavGf2jx0/view?usp=sharing" className='button' target="resume" rel="noopener noreferrer" style={{ color: 'black' }}>
+        <a href="https://drive.google.com/file/d/1Yi5X5wtittnBsjQxMERBc-lJEdnSWwKT/view?usp=sharing" className='button' target="resume" rel="noopener noreferrer" style={{ color: 'black' }}>
           RESUME
         </a>
       </div>
