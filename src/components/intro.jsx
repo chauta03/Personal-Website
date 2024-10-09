0b10533398c6e17713fc4d9089d1cbb9230e93f4
tachau03 <taminhchau8903@gmail.com>
2024-10-09 08:39:33 -0400

    Update intro.jsx

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -6,7 +6,7 @@ const Intro = () => {
         Bring impacts to life
       </div>
       <div className="text2">
-        I work on purpose-driven projects as software engineer that have positive impacts on the companies. <a>Currently looking for an internship of Summer 2025.</a>
+        I work on purpose-driven projects as software engineer that have positive impacts on the companies. <a>Currently looking for full-time position of 2025.</a>
       </div>
     </div>
   );
